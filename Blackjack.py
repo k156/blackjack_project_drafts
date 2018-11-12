63f0b1051a37da00ece77c2aa8613c54101e9cff
k156 <jm_91@live.co.kr>
2018-11-12 18:10:17 +0900

    blackjack

--- Blackjack.py
+++ Blackjack.py
@@ -8,17 +8,30 @@ class Casting:
 class Deck:
     def randomcard ():
         import random
-        deck = [heart1,2,3,4,spade1,2,3,4,diamond1,2,3,4,clover1,2,3,4]
-
+        deck = ['sa','s2','s3','s4','s5','s6','s7','s8','s9','s10','sj','sq','sk','ca','c2','c3','c4','c5','c6','c7','c8','c9','c10','cj','cq','ck','ha','h2','h3','h4','h5','h6','h7','h8','h9','h10','hj','hq','hk','da','d2','d3','d4','d5','d6','d7','d8','d9','d10','dj','dq','dk']
+        random.choice(deck)
 
 
 class Game:
     while (card1 + card2 + ... <21):     
         def start_game(self):
             card = Deck.randomcard(deck)
+            
+            card.lstrip('s')
+            card.lstrip('c')
+            card.lstrip('h')
+            card.lstrip('d')
+            if card.find ('a'):
+                a_value = input("A값을 1과 11중에 선택하세요.")
+                if a_value == 1:
+                    cardlist.append(1)
+                if a_value == 11:
+                    cardlist.append(2)
+            if card.find('j,q,k'):
+                cardlist.append(10)
             Casting.to_int(card)
-            list = ()
-            list.append(card)
+            cardlist = []
+            cardlist.append(card)
 
 
 class Player(Game):
@@ -28,9 +41,8 @@ class Player(Game):
         if list의합 > 21:
             print("패")
         if list의합 <21:
-            hitorstand = input("Hit 하고싶으면 1, Stand 하고싶으면 2를 입력하세요.")
-            if hitorstand = 1:
-                continue
+            super()
+            hit = input("카드를 더 받으려면 'hit', 그만 받으려면 'stand'를 입력하세요.")
             if hitorstand = 2:
                 break
                 if 딜러 < 플레이어:
---
+++ temp.py
@@ -0,0 +1,4 @@
+num = ["a1", "a2", "a3", "b1", "b2", "b3"]
+for i in num:
+    print(i.lstrip('a'))
+    
